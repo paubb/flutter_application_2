10301f43a251b57bd26b38d478023650506a610a
pau.ballber <pau.ballber@est.fib.upc.edu>
2022-09-14 18:20:59 +0200

    Login page redesign

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -13,40 +13,88 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: (isLoading)
-            ? CircularProgressIndicator()
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      await FireAuth.registerUsingEmailPassword(
-                          "pauas", "pballber@hotmail.com", "123456789");
-                    },
-                    child: Text(
-                      'Sign up',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      await FireAuth.signInUsingEmailPassword(
-                          "pballber@hotmail.com", "123456789");
-                    },
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
               ),
+              child: (isLoading)
+                  ? Align(
+                      child: CircularProgressIndicator(),
+                      alignment: Alignment.center,
+                    )
+                  : IntrinsicHeight(
+                      child: Column(
+                        children: [
+                          Spacer(),
+                          Text(
+                            "Welcome",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 25),
+                          ),
+                          SizedBox(height: 25),
+                          Text(
+                            "Explore the application",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          SizedBox(height: 50),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(width: 50),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    await FireAuth.registerUsingEmailPassword(
+                                        "pauas",
+                                        "pballber@hotmail.com",
+                                        "123456789");
+                                  },
+                                  child: Text('Sign up'),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          10), // <-- Radius
+                                    ),
+                                    padding: const EdgeInsets.all(15),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () async {
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    await FireAuth.signInUsingEmailPassword(
+                                        "pballber@hotmail.com", "123456789");
+                                  },
+                                  child: Text('Sign in'),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          10), // <-- Radius
+                                    ),
+                                    padding: const EdgeInsets.all(15),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 50),
+                            ],
+                          ),
+                          SizedBox(height: 75),
+                        ],
+                      ),
+                    ),
+            ),
+          );
+        },
       ),
     );
   }
