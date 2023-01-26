cc93ffb4520a9bea29008f91f84f846f6f51a203
pau.ballber <pau.ballber@est.fib.upc.edu>
2023-01-26 11:13:54 +0100

    Refactor welcome view

--- lib/views/welcome.dart
+++ lib/views/welcome.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/config/constants.dart';
 
-class WelcomePage extends StatefulWidget {
-  WelcomePage({Key key}) : super(key: key);
-
-  @override
-  _WelcomePageState createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage> {
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,62 +16,71 @@ class _WelcomePageState extends State<WelcomePage> {
                 child: Column(
                   children: [
                     Spacer(),
-                    Text(
+                    const Text(
                       "Welcome",
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                     ),
-                    SizedBox(height: 25),
-                    Text(
+                    const SizedBox(height: 25),
+                    const Text(
                       "Explore the application",
                       style: TextStyle(fontSize: 15),
                     ),
-                    SizedBox(height: 50),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(width: 50),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              Navigator.pushNamed(context, signupRoute);
-                            },
-                            child: Text('Sign up'),
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.circular(10), // <-- Radius
-                              ),
-                              padding: const EdgeInsets.all(15),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              Navigator.pushNamed(context, signinRoute);
-                            },
-                            child: Text('Sign in'),
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.circular(10), // <-- Radius
-                              ),
-                              padding: const EdgeInsets.all(15),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 50),
-                      ],
+                    const SizedBox(height: 50),
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(child: SignUpViewButton()),
+                          const SizedBox(width: 10),
+                          Expanded(child: SignInViewButton()),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 75),
+                    const SizedBox(height: 75),
                   ],
                 ),
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class SignUpViewButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        Navigator.pushNamed(context, signupRoute);
+      },
+      child: Text('Sign up'),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10), // <-- Radius
+        ),
+        padding: const EdgeInsets.all(15),
+      ),
+    );
+  }
+}
+
+class SignInViewButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        Navigator.pushNamed(context, signinRoute);
+      },
+      child: Text('Sign in'),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10), // <-- Radius
+        ),
+        padding: const EdgeInsets.all(15),
       ),
     );
   }
